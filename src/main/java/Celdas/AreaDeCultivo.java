bdf357a318c4661bbc13e4141533a9f3316af13b
JorgeRomojaro11 <jromodel@myuax.com>
2024-05-21 22:56:53 +0200

    Cultivo

--- src/main/java/Celdas/AreaDeCultivo.java
+++ src/main/java/Celdas/AreaDeCultivo.java
@@ -1,20 +1,49 @@
 package Celdas;
 
+import Bacteria.Bacteria;
+
+import java.util.Random;
+
 public class AreaDeCultivo {
     private Celda[][] celdas;
+    private Random random;
 
     public AreaDeCultivo(int tamaño) {
         this.celdas = new Celda[tamaño][tamaño];
+        this.random = new Random();
         // Inicializar las celdas
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                celdas[i][j] = new Celda();
+            }
+        }
     }
 
     public void inicializar(int cantidadBacterias, int comida) {
-        // Implementar la lógica para inicializar el área de cultivo
+        // Distribuir bacterias y comida de manera aleatoria
+        for (int i = 0; i < cantidadBacterias; i++) {
+            int x = random.nextInt(celdas.length);
+            int y = random.nextInt(celdas[0].length);
+            celdas[x][y].agregarBacteria(new Bacteria(celdas[x][y]));
+        }
+        for (int i = 0; i < comida; i++) {
+            int x = random.nextInt(celdas.length);
+            int y = random.nextInt(celdas[0].length);
+            celdas[x][y].agregarComida(1);
+        }
     }
 
     public void simularDia() {
-        // Implementar la lógica para simular un día
+        // Hacer que cada bacteria en cada celda se mueva, coma, se reproduzca y verifique si muere
+        for (Celda[] fila : celdas) {
+            for (Celda celda : fila) {
+                for (Bacteria bacteria : celda.getBacterias()) {
+                    bacteria.mover();
+                    bacteria.comer(celda.obtenerComida());
+                    bacteria.reproducir();
+                    bacteria.verificarSiMuere();
+                }
+            }
+        }
     }
-
-    // Otros métodos necesarios
 }
